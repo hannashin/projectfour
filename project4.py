a79ef5d001439926eaf1dc6e5f1105c7b53e2d7e
Han Na Shin <hanshin@Hans-MacBook-Pro-5.local>
2016-11-27 23:54:09 -0500

    Woohoo

--- project4.py
+++ project4.py
@@ -41,6 +41,18 @@ class Homework(Sprite):
         randY = randint(0, 200)
         self.rect.center = (randX,randY)
 
+class Eightthirty(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("media/eight_thirty.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    # move gold to a new random location
+    def move(self):
+        randX = randint(0, 150)
+        randY = randint(100, 150)
+        self.rect.center = (randX,randY)
+
 class Student(Sprite):
     def __init__(self):
         Sprite.__init__(self)
@@ -56,6 +68,15 @@ class Student(Sprite):
     def update(self):
         self.rect.center = mouse.get_pos()
 
+class PencilBullet(Sprite):
+    def __init__(self):
+        Sprite.__init__(self, 'media/pencil.png')
+        self.image=pygame.transform.scale(self.image,(8,12))
+        self.width = 8
+        self.height = 12
+        self.bullet = True
+        laser.play()
+
 #---------prints in terminal befoer game starts------------
 print ("""Your goal is to hit all of the daily tasks as college students we are trying to avoid. Have fun!""")
 
@@ -66,6 +87,10 @@ if student_name == "":
 #main
 pygame.init()
 
+#background music infinite loop
+pygame.mixer.music.load("media/run_song.wav")
+pygame.mixer.music.play(-1)
+
 screen = display.set_mode((640, 480))
 display.set_caption('College Student Daily Struggles')
 
@@ -74,12 +99,16 @@ mouse.set_visible(False)
 
 f = font.Font(None, 18)
 
-# create the mole and shovel using the constructors
+# constructions of the enemy images
 snowflake = Snowflake()
 homework = Homework()
+eightthirty = Eightthirty()
 student = Student()
+
 # creates a group of sprites so all can be updated at once
 sprites = RenderPlain(snowflake, student)
+sprites1 = RenderPlain(homework, student)
+sprites2 = RenderPlain(eightthirty, student)
 
 hits = 0
 timer_zone = time.set_timer(USEREVENT + 1, DELAY)
@@ -100,17 +129,29 @@ while True:
             snowflake.move()
             hits += 1
 
+        if student.hit(homework):
+            mixer.Sound("media/okay_bye.wav").play()
+            homework.move()
+            hits += 1
+
+        if student.hit(eightthirty):
+            mixer.Sound("media/okay_bye.wav").play()
+            eightthirty.move()
+            hits += 1
+
             # reset timer
             time.set_timer(USEREVENT + 1, DELAY)
             
     elif e.type == USEREVENT + 1: # TIME has passed
         snowflake.move()
+        homework.move()
+        eightthirty.move()
 
     # refill background color so that we can paint sprites in new locations
     screen.blit(backgroundimage, [0,0])
     t = f.render("Stuck with:" + str(hits), False, (255,255,255))
     y = f.render("Time: = " + str(clock), False, (255,255,255))
-    z = f.render('Name:' + str(student_name), False, (255,255,255))
+    z = f.render('Student:' + str(student_name), False, (255,255,255))
     screen.blit(z, (0, 0))
     screen.blit(t, (0, 20)) 
     screen.blit(y, (0, 40))
@@ -118,12 +159,16 @@ while True:
 
     # update and redraw sprites
     sprites.update()
+    sprites1.update()
+    sprites2.update()
     sprites.draw(screen)
+    sprites1.draw(screen)
+    sprites2.draw(screen)
     display.update()
     clock.tick(60)
 
 #----------END OF TERMINAL SHOWING SCORES WHEN GAME QUIT-------
 
 print (str(student_name) + ", you avoided a lot of daily struggles!")
-print ('You avoided' + str(hits) + 'tasks!')
+print ('You avoided ' + str(hits) + ' tasks!')
 print ('END OF GAME - GET BACK TO WORK!')
