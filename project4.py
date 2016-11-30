854a056185743fa7e7c37662546d226ca98537bc
Han Na Shin <hanshin@Hans-MacBook-Pro-5.local>
2016-11-30 10:55:26 -0500

    Progress

--- project4.py
+++ project4.py
@@ -3,7 +3,7 @@ from pygame.sprite import *
 from random import *
 
 #Timer to move sprite
-DELAY = 900; #speed of the targets          
+DELAY = 1200; #speed of the targets          
 
 #Colors 
 backgroundimage = pygame.image.load("images/campus.jpg")
@@ -33,8 +33,8 @@ class Homework(Sprite):
 
     # move gold to a new random location
     def move(self):
-        randX = randint(0, 400)
-        randY = randint(0, 200)
+        randX = randint(0, 600)
+        randY = randint(0, 600)
         self.rect.center = (randX,randY)
 
 class Eightthirty(Sprite):
@@ -45,9 +45,34 @@ class Eightthirty(Sprite):
 
     # move gold to a new random location
     def move(self):
-        randX = randint(0, 150)
-        randY = randint(100, 150)
+        randX = randint(0, 600)
+        randY = randint(100, 600)
         self.rect.center = (randX,randY)
+
+#Target Images -----
+class Coffee(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("images/coffee.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        randX = randint(0, 600)
+        randY = randint(0, 600)
+        self.rect.center = (randX,randY)
+
+class Aplus(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("images/aplus.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        randX = randint(0, 600)
+        randY = randint(0, 600)
+        self.rect.center = (randX,randY)
+
+#Student Image ------
 
 class Student(Sprite):
     def __init__(self):
@@ -62,16 +87,6 @@ class Student(Sprite):
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
-
-class PencilBullet(Sprite):
-    def __init__(self):
-        Sprite.__init__(self, 'images/pencil.png')
-        self.image=pygame.transform.scale(self.image,(8,12))
-        self.width = 8
-        self.height = 12
-        self.bullet = True
-        laser.play()
-
 #---------prints in terminal befoer game starts------------
 print ("""Your goal is to hit all of the daily tasks as college students we are trying to avoid. Have fun!""")
 
@@ -81,14 +96,13 @@ if student_name == "":
 
 #main
 pygame.init()
-arrows = []
 
 #background music infinite loop
 pygame.mixer.music.load("songs/run_song.wav")
 pygame.mixer.music.play(-1)
 
 screen = display.set_mode((800, 640))
-display.set_caption('College Student Daily Tasks')
+display.set_caption('Student Boost Pack')
 
 pygame.mouse.set_visible(False)
 
@@ -99,11 +113,15 @@ snowflake = Snowflake()
 homework = Homework()
 eightthirty = Eightthirty()
 student = Student()
+coffee = Coffee()
+aplus = Aplus()
 
 # creates a group of sprites so all can be updated at once
 sprites = RenderPlain(snowflake, student)
 sprites1 = RenderPlain(homework, student)
 sprites2 = RenderPlain(eightthirty, student)
+sprites3 = RenderPlain(coffee, student)
+sprites4 = RenderPlain (aplus, student)
 
 hits = 0
 timer_zone = time.set_timer(USEREVENT + 1, DELAY)
@@ -111,7 +129,6 @@ timer_zone = time.set_timer(USEREVENT + 1, DELAY)
 #clock
 clock = pygame.time.Clock()
 
-# loop until user quits
 while True:
     clock.tick(30)
     x = event.poll()
@@ -127,42 +144,62 @@ while True:
             hits += 0
 
         if student.hit(homework):
-            homework.move()
+            snowflake.move()
             hits += 0
 
         if student.hit(eightthirty):
-            eightthirty.move()
+            snowflake.move()
+            hits += 0
+
+        if student.hit(coffee):
+            coffee.move()
+            hits += 0   
+
+        if student.hit(aplus):
+            aplus.move()
             hits += 0        
 
     elif x.type == MOUSEBUTTONDOWN:
         if student.hit(snowflake):
             mixer.Sound("songs/ohmygod.wav").play()
             snowflake.move()
-            hits += 1
+            hits -= 1
 
         if student.hit(homework):
             mixer.Sound("songs/okay_bye.wav").play()
             homework.move()
-            hits += 1
+            hits -= 1
 
         if student.hit(eightthirty):
             mixer.Sound("songs/okay_bye.wav").play()
             eightthirty.move()
+            hits -= 1
+
+        if student.hit(coffee):
+            mixer.Sound("songs/woohoo.wav").play()
+            coffee.move()
+            hits += 1
+
+        if student.hit(aplus):
+            mixer.Sound("songs/woohoo.wav").play()
+            aplus.move()
             hits += 1
 
             # reset timer
             time.set_timer(USEREVENT + 1, DELAY)
             
-    elif x.type == USEREVENT: # TIME has passed
+    elif x.type == USEREVENT + 1: # TIME has passed
         snowflake.move()
         homework.move()
         eightthirty.move()
+        coffee.move()
+        aplus.move()
 
     # refill background color so that we can paint sprites in new locations
     screen.blit(backgroundimage, [0,0])
-    t = f.render("Stuck with:" + str(hits), False, (255,255,255))
+    t = f.render("Boost Pack:" + str(hits), False, (255,255,255))
     y = f.render("Time: = " + str(clock), True, (255,255,255))
-    z = f.render('Student:' + str(student_name), False, (255,255,255))
+    z = f.render('Student Name:' + str(student_name), False, (255,255,255))
     screen.blit(z, (0, 0))
     screen.blit(t, (0, 20)) 
     screen.blit(y, (0, 40))
@@ -172,14 +209,18 @@ while True:
     sprites.update()
     sprites1.update()
     sprites2.update()
+    sprites3.update()
+    sprites4.update()
     sprites.draw(screen)
     sprites1.draw(screen)
     sprites2.draw(screen)
+    sprites3.draw(screen)
+    sprites4.draw(screen)
     display.update()
 
 #----------END OF TERMINAL SHOWING SCORES WHEN GAME QUIT-------
 
 print ("------------------------------------------")
-print (str(student_name) + ", you avoided a lot of daily tasks!")
-print ('You avoided ' + str(hits) + ' tasks!')
+print (str(student_name) + ", this is your boost pack score for the day!")
+print ('You got ' + str(hits) + ' boost pack points!')
 print ('END OF GAME - GET BACK TO WORK!')
--- project4_206.py
+++ project4_206.py
@@ -2,8 +2,9 @@ from pygame import *
 from pygame.sprite import *
 from random import *
 
+
 #Timer to move sprite
-DELAY = 900; #speed of the targets          
+DELAY = 1200; #speed of the targets          
 
 #Colors 
 backgroundimage = pygame.image.load("images/campus.jpg")
@@ -14,14 +15,13 @@ green = (0, 200, 0)
 yellow = (255, 255, 0)   
 
 
-
+#Avoiding Image Classes
 class Snowflake(Sprite):
     def __init__(self):
         Sprite.__init__(self)
         self.image = image.load("images/snowflake.png").convert_alpha()
         self.rect = self.image.get_rect()
 
-    # move gold to a new random location
     def move(self):
         randX = randint(0, 600)
         randY = randint(0, 400)
@@ -33,7 +33,6 @@ class Homework(Sprite):
         self.image = image.load("images/homework.png").convert_alpha()
         self.rect = self.image.get_rect()
 
-    # move gold to a new random location
     def move(self):
         randX = randint(0, 400)
         randY = randint(0, 200)
@@ -45,12 +44,36 @@ class Eightthirty(Sprite):
         self.image = image.load("images/eight_thirty.png").convert_alpha()
         self.rect = self.image.get_rect()
 
-    # move gold to a new random location
     def move(self):
         randX = randint(0, 150)
         randY = randint(100, 150)
         self.rect.center = (randX,randY)
 
+#Target Image Classes
+class Coffee(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("images/coffee.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        randX = randint(0, 300)
+        randY = randint(0, 116)
+        self.rect.center = (randX,randY)
+
+
+class APlus(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("images/aplus.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        randX = randint(0, 222)
+        randY = randint(0, 222)
+        self.rect.center = (randX, randY)
+
+#Student class
 class Student(Sprite):
     def __init__(self):
         Sprite.__init__(self)
@@ -62,17 +85,8 @@ class Student(Sprite):
         return self.rect.colliderect(target)
 
     def update(self):
-        self.rect.center = pygame.mouse.get_pos()
+        self.rect.center = pygame.mouse.get_pos() #should follow mouse around
 
-
-class PencilBullet(Sprite):
-    def __init__(self):
-        Sprite.__init__(self, 'images/pencil.png')
-        self.image=pygame.transform.scale(self.image,(8,12))
-        self.width = 8
-        self.height = 12
-        self.bullet = True
-        laser.play()
 
 #---------prints in terminal befoer game starts------------
 print ("""Your goal is to hit all of the daily tasks as college students we are trying to avoid. Have fun!""")
@@ -95,16 +109,22 @@ pygame.mouse.set_visible(False)
 
 f = font.Font(None, 18)
 
-# constructions of the enemy images
+# constructions of the avoiding images
 snowflake = Snowflake()
 homework = Homework()
 eightthirty = Eightthirty()
 student = Student()
 
+#constructions of the target images
+coffee = Coffee()
+aplus = APlus()
+
 # creates a group of sprites so all can be updated at once
 sprites = RenderPlain(snowflake, student)
 sprites1 = RenderPlain(homework, student)
 sprites2 = RenderPlain(eightthirty, student)
+sprites3 = RenderPlain(coffee, student)
+sprites4 = RenderPlain(aplus, student)
 
 hits = 0
 timer_zone = time.set_timer(USEREVENT + 1, DELAY)
@@ -122,7 +142,7 @@ while True:
         break
 
     elif x.type == MOUSEMOTION:
-        #
+        
         if student.hit(snowflake):
             snowflake.move()
             hits += 0
@@ -133,31 +153,52 @@ while True:
 
         if student.hit(eightthirty):
             eightthirty.move()
+            hits += 0
+
+        if student.hit(coffee):
+            coffee.move()
             hits += 0        
 
+        if student.hit(aplus):
+            aplus.move()
+            hits += 0
+
     elif x.type == MOUSEBUTTONDOWN:
+
         if student.hit(snowflake):
             mixer.Sound("songs/ohmygod.wav").play()
             snowflake.move()
-            hits += 1
+            hits -= 1 
 
         if student.hit(homework):
             mixer.Sound("songs/okay_bye.wav").play()
             homework.move()
-            hits += 1
+            hits -= 1
 
         if student.hit(eightthirty):
             mixer.Sound("songs/okay_bye.wav").play()
             eightthirty.move()
+            hits -= 1
+
+        if student.hit(coffee):
+            mixer.Sound('songs/woohoo.wav').play()
+            coffee.move()
+            hits += 1
+
+        if student.hit(aplus):
+            mixer.Sound('songs/woohoo.wav').play()
+            aplus.move()
             hits += 1
 
             # reset timer
             time.set_timer(USEREVENT + 1, DELAY)
             
-    elif x.type == USEREVENT: # TIME has passed
+    elif x.type == USEREVENT:
         snowflake.move()
         homework.move()
         eightthirty.move()
+        coffee.move()
+        aplus.move()
 
     # refill background color so that we can paint sprites in new locations
     screen.blit(backgroundimage, [0,0])
@@ -173,9 +214,13 @@ while True:
     sprites.update()
     sprites1.update()
     sprites2.update()
+    sprites3.update()
+    sprites4.update()
     sprites.draw(screen)
     sprites1.draw(screen)
     sprites2.draw(screen)
+    sprites3.draw(screen)
+    sprites4.draw(screen)
     display.update()
 
 #----------END OF TERMINAL SHOWING SCORES WHEN GAME QUIT-------
